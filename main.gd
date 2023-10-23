d3e49a8a508cfdda978127975b24e83e2801c8f1
arnfarkas <arn.farkas@gmail.com>
2023-10-23 11:06:29 +0200

    change start ui

--- main.gd
+++ main.gd
@@ -10,3 +10,11 @@ func _on_quit_pressed():
 
 func _on_play_pressed():
 	get_tree().change_scene_to_file("res://world.tscn") # Replace with function body.
+
+func _process(_delta):
+	var screen_size = get_viewport_rect().size
+	var center = Vector2(screen_size.x / 2, screen_size.y / 3)
+	
+	var container: Container = $CenterContainer
+	var offset = container.size / 2
+	container.global_position = center - offset
