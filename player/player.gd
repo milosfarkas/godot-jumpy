7e852816fd51edc28b2f72e7484d5db1f6cf3299
arnfarkas <arn.farkas@gmail.com>
2023-10-25 22:50:25 +0200

    android butto jump

--- player/player.gd
+++ player/player.gd
@@ -13,7 +13,7 @@ func _physics_process(delta):
 		velocity.y += gravity * delta
 
 	# Handle Jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 		animation.play("jump")
 		
